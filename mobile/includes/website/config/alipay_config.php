7363307151a181debd7a50234707fef0cd7fc839
liuzhiqi073345 <573003557@qq.com>
2016-03-06 18:16:21 +0800

    Revert "benyondgo_mobile"
    This reverts commit 8d6cbd4c87ba90b519d6f8bc49f87c3ccdefcbd0.

--- mobile/includes/website/config/alipay_config.php
+++
@@ -1,8 +0,0 @@
-<?php 
- // 第三方插件登录信息---------------------
-define('RANK_ID' , '4'); 
-define('APP_KEY' , '2015071800176460'); 
-$config['APP_KEY'] = '2015071800176460'; 
-define('APP_SECRET' , '7pj7ehl1ndzsxym72fr97d81fml8o0rz'); 
-$config['APP_SECRET'] = '7pj7ehl1ndzsxym72fr97d81fml8o0rz'; 
-?>
